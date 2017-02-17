10f89683336dc59bd349374b96f0ee7c31f5ba88
Jun Lin <wate11111@gmail.com>
2017-02-16 18:23:05 -0600

    Final project

--- src/cpuscheduler/CPUScheduler.java
+++ src/cpuscheduler/CPUScheduler.java
@@ -3,8 +3,8 @@ import java.util.Random;
 
 /**
  * A simple non-preemptive CPU Scheduler
- * @author Your Name
- * @since date this file was last modified
+ * @author Jun Lin
+ * @since 02/14/2017
  */
 
 
@@ -12,9 +12,59 @@ public class CPUScheduler
 {
    public static void main(String []args) throws HeapException
    {
-      
-      //int randInt = generator.nextTint(-20,19);
-      //while()
+      Random generator = new Random();
+      Heap<PCB>pq = new Heap();
+      int S = Integer.parseInt(args[0]);
+      float p = Float.parseFloat(args[1]);
+      int count = 0;
+      int waitTotal = 0;
+      int i = 1;
+      int completedProcesses = 0;
+      int priority, length, arrival, running;
+      float q;
+      while (i <= S)
+      {
+         System.out.printf("Cycle #: %d ***%n", i);
+         q = generator.nextFloat();
+         if (q < p)
+         {
+            count++;
+            priority = generator.nextInt(40)- 19;
+            length = generator.nextInt(101);
+            arrival = i;
+            running = 0;
+            PCB thePCB = new PCB(count, priority, running, arrival, length);
+            pq.insert(thePCB);
+            System.out.printf("Adding job with pid #%d and priority %d and length %d%n", count,priority, length);
+         }
+         if (!pq.isEmpty())
+         {
+            if(!pq.peek().isExecuting())
+            {
+               pq.peek().execute();
+               pq.peek().setStart(i);
+               pq.peek().setWait(i - pq.peek().getArrival());
+               waitTotal += pq.peek().getWait();
+            }
+            if (i-pq.peek().getStart() == pq.peek().getLength())
+            {
+               PCB temp = pq.peek();
+               pq.remove();
+               System.out.printf("Process # %d has just terminated. %n", temp.getPid());
+               completedProcesses++;
+            }
+            else
+            {
+               System.out.printf("Procress # %d is executing %n", pq.peek().getPid());
+            }
+         }
+         System.out.println("No new job this cycle");
+         i++;
+      }
+      double averageTurn = (double) completedProcesses / (double) S;
+      System.out.printf("The average turn around is %.3f per cycle. %n",averageTurn);
+      double averageWait = (double) waitTotal/(double) completedProcesses;
+      System.out.printf("The average wait time is %.6f%n",averageWait);
 
    }
 }
